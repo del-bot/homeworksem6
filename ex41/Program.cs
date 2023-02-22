dcf08c219bbd1457f08ded05ae24b60b331d7a1a
Delyash <delyash91@mail.ru>
2023-02-22 14:55:51 -0500

    ex41

---
+++ ex41/Program.cs
@@ -0,0 +1,19 @@
+﻿void Main()
+{
+    Console.Write("M = ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int[] a = new int[n];
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+    {
+        Console.Write($"A[{i + 1}] = ");
+        a[i] = Convert.ToInt32(Console.ReadLine());
+        if(a[i] > 0)
+        {
+            sum++;
+        }
+    }
+    Console.WriteLine(sum);
+
+}
+Main();
